d8f4ba0ba53c831909c35b2f1ec52a21180acc3e
刘雨春 <1756303287@qq.com>
2020-08-11 15:07:24 +0800

    master新增了newmasterModel

---
+++ 第三方字体/第三方字体/newmasterModel.h
@@ -0,0 +1,17 @@
+//
+//  newmasterModel.h
+//  第三方字体
+//
+//  Created by a on 2020/8/11.
+//  Copyright © 2020 刘雨春. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface newmasterModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
