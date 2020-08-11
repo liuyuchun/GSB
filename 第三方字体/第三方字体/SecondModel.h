6e286d5b6828fc1e52692ceabc857f66a8d8f58b
刘雨春 <1756303287@qq.com>
2020-08-11 18:05:13 +0800

    1、新建secondModel

---
+++ 第三方字体/第三方字体/SecondModel.h
@@ -0,0 +1,17 @@
+//
+//  SecondModel.h
+//  第三方字体
+//
+//  Created by a on 2020/8/11.
+//  Copyright © 2020 刘雨春. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SecondModel : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
